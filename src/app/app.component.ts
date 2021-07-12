8e7a1770657618b65422020837096796a23bc053
Tushar Kumar <Tushar.Kumar@pixere.local>
2021-07-12 11:20:35 +0530

    covid19

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpService } from './http.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  constructor(private _httpService: HttpService) {}
+
   title = 'covid19App';
+  apiSummary;
+  apiLocation=[];
+
+  ngOnInit(): void {
+    this._httpService.getCovid19Detail().subscribe(Response =>{
+      this.apiLocation = Response.data.regional;
+      console.log(this.apiSummary);
+    });
+  }
+
 }
